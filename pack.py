7627f7ad162c7d055c5274fbb7096af98e73de93
nobuyoshi <c0b23081fb@edu.teu.ac.jp>
2024-11-12 16:19:09 +0900

    完全最終コード

--- pack.py
+++ pack.py
@@ -336,6 +336,8 @@ clock = pygame.time.Clock()
 running = True
 while running:
     for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:  # スペースキーでビームを発射
                 fire_beam()
